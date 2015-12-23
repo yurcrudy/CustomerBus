9e0f5151d09f9e833f3f60296294973852e51278
yujo2 <jonas.r.c.yu@cargosmart.com>
2015-12-23 16:54:46 +0800

    CustomerBus init Project

---
+++ app/src/main/java/com/yurc/customerbus/activity/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.yurc.customerbus.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Date：12/23/2015
+ * Author：yurc
+ * Describe：all Activity extends this Activity
+ */
+public class BaseActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/yurc/customerbus/activity/TestActivity.java
@@ -0,0 +1,21 @@
+package com.yurc.customerbus.activity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import com.yurc.customerbus.R;
+
+import org.androidannotations.annotations.EActivity;
+
+/**
+ * Date：12/23/2015
+ * Author：yurc
+ * Describe：
+ */
+@EActivity(R.layout.activity_test)
+public class TestActivity extends BaseActivity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
