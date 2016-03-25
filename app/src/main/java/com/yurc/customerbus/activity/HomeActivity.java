24e6ff0fa85659dd56ca37e6342edb340dc07268
yurc_rudy@163.com <yrc289898543>
2016-03-25 17:23:46 +0800

    add actionbar default style add home activity

---
+++ app/src/main/java/com/yurc/customerbus/activity/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.yurc.customerbus.activity;
+
+import android.os.Bundle;
+
+/**
+ * Date：3/24/2016
+ * Author：yurc
+ * Describe：
+ */
+public class HomeActivity extends BaseActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
